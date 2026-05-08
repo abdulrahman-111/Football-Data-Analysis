1fb8bc6015ebeaf659fa2c7ecd6e77e608a466a7
abdo <abdulrhman.gomaa.h05@gmail.com>
2026-05-08 15:34:00 +0300

    update player report file

--- src/player_report.py
+++
@@ -1,6 +0,0 @@
-# we want to use the pretrained model for Injury Classifier and Goals and Assist in next season and 
-# Transfer Market value to create a report for the player to use this report to assess if player fit our team or not , make sure to make decision based on score for each target we have 
-# for each model it takes specific paramters , we want to accept input from JSON file with values for each feature 
-# here is the feature for each model 
-
-# next_season_goals 
